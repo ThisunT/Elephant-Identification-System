f8ddc201f2af83c35b2c5a1d4167099ab01c036c
Unknown <lalangaariyasinghe@gmail.com>
2018-08-18 16:41:52 +0530

    Commited Account Update

--- front-end/src/views/Authentication/AccountUpdate.jsx
+++ front-end/src/views/Authentication/AccountUpdate.jsx
@@ -1,79 +1,156 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import AuthUserContext from './AuthUserContext';
 import withAuthorization from './withAuthorization';
 import Paper from 'material-ui/Paper';
 import TextField from 'material-ui/TextField';
 import {Link} from 'react-router-dom';
 import Button from "@material-ui/core/es/Button/Button";
-
+import GridItem from "../../components/Grid/GridItem";
+import CustomInput from "../../components/CustomInput/CustomInput";
+//import * as database from "../../firebase/databse";
+import {auth, database} from '../../firebase/index';
 
 
 const font = "'Varela Round', sans-serif";
 
 const style = {
-      height: '750px',
-      width: '550px',
+      height: '800px',
+      width: '650px',
       margin: '10%',
       textAlign: 'center',
       display: 'inline-block',
+
 };
 
-const AccountUpdatePage = () =>
-      <AuthUserContext.Consumer>
-            {authUser =>
+const styleN = {
+      fontSize : '2em',
 
-                  <div>
-                        <div className="row">
-                              <div className="col-lg-4">
-                                    <img src={require('../../images/profile.jpg')} width="2800px" />
-                              </div>
+};
 
-                              <div className="col-lg-4">
+const gridStyle ={
+      marginLeft: '12em'
+};
+
+
+const byPropKey = (propertyName, value) => () => ({
+    [propertyName]: value,
+});
+const INITIAL_STATE = {
+
+    about: '',
+    country: '',
+    institute: '',
+};
+
+
+
+this.updateAccount = function (event) {
+    //this.handleClose();
+    database.doCreateUser();
+    window.location.reload();
+}
+class AccountUpdatePage extends Component {
+
+    constructor(props){
+        super(props);
+
+        //this.state = { ...INITIAL_STATE };
+
+         this.state = {
+
+            about : '',
+            country : '',
+            institute: ''
+        }
+
+    }
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+    updateAccount(event) {
+        this.handleClose();
+        console.log(this.state);
+        console.log(this.state.about);
+        database.doUpdateUser(auth.getUser().uid,this.state.about,this.state.country,this.state.institute);
+        window.location.reload();
+
+    }
+    render() {
+        const {
+            about,
+            country,
+            institute,
+        } = this.state;
+        //console.log(auth.getUser());
+        return (
+            <div>
+                <AuthUserContext.Consumer>
+
+                    {authUser =>
+
+                        <div>
+                            <div className="row">
+                                <div className="col-lg-4">
+                                    <img src={require('../../images/profile.jpg')} width="1500"/>
+                                </div>
+
+                                <div className="col-lg-4">
                                     <div align="center">
-                                          <Paper style={style} zDepth={2} >
-                                                <h1> My Account: {authUser.email}</h1>
-                                                <br />
-                                                <h1>PROFILE</h1>
-                                                <br />
-                                                <TextField
-                                                      hintText="User Name"
-                                                /><br />
-                                                <TextField
-                                                      hintText="Birthday"
-                                                /><br />
-                                                <TextField
-                                                      hintText="About"
-                                                /><br />
-                                                <TextField
-                                                      hintText="College"
-                                                /><br />
-                                                <TextField
-                                                      hintText="Country"
-                                                /><br />
-                                                <TextField
-                                                      hintText="Processes"
-                                                /><br />
-                                                <TextField
-                                                      hintText="Locations"
-                                                /><br />
+                                        <Paper style={style} zDepth={2}>
+                                            <h1 style={styleN}> My Account: {authUser.email}</h1>
+                                            <br/>
+                                            <h1>Profile</h1>
+                                            <br/>
+                                            {/*<TextField*/}
+                                                {/*onChange={event => this.setState(byPropKey('username', event.target.value))}*/}
+                                                {/*type="text"*/}
+                                                {/*placeholder="User Name"*/}
+                                            {/*/><br/>*/}
 
-                                                <Button primary={true} style={{fontFamily: font, margin: '5%'}} type="submit">
-                                                      Update
-                                                </Button>
+                                            <TextField
+                                                value={about}
+                                                onChange={event => this.setState(byPropKey('about', event.target.value))}
+                                                type="text"
+                                                placeholder="About"
+                                            /><br/>
 
-                                                <Link to={"/profile"}>
-                                                <Button primary={true} style={{fontFamily: font, margin: '5%'}} >
-                                                      Back
-                                                </Button>
-                                                </Link>
-                                          </Paper>
+                                            <TextField
+                                                value={country}
+                                                onChange={event => this.setState(byPropKey('country', event.target.value))}
+                                                type="text"
+                                                placeholder="Country"
+                                            /><br/>
+
+                                            <TextField
+                                                value={institute}
+                                                onChange={event => this.setState(byPropKey('institute', event.target.value))}
+                                                type="text"
+                                                placeholder="Institute"
+                                            /><br/>
+
+
+                                            <br/>
+
+                                            <Button type="button" color="primary"
+                                                    onClick={(event) => this.updateAccount(event)}
+                                                    round>Update</Button>
+
+                                            <Link to={"/profile"}>
+                                                <Button type="button" color="primary" round>Back</Button>
+                                            </Link>
+                                        </Paper>
                                     </div>
-                              </div>
-                        </div>
+                                </div>
+                            </div>
 
-                  </div>
-            }
-      </AuthUserContext.Consumer>
+                        </div>
+                    }
+                </AuthUserContext.Consumer>
+            </div>
+        )
+    }
+}
 
 
 const authCondition = (authUser) => !!authUser;
